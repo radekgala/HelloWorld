96560ef1662ec0ea741ecf4ff8d983d8751d6c30
radoslaw.gala <radoslaw.gala@PLKTW4649N>
2016-10-18 11:57:40 +0200

    add nunit

--- UnitTestProject1/UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -1,12 +1,13 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+using NUnit.Framework;
 namespace UnitTestProject1
 {
-    [TestClass]
+
+    [TestFixture]
     public class UnitTest1
     {
-        [TestMethod]
+        [Test]
         public void TestMethod1()
         { //test branch
             Assert.IsTrue(true);
